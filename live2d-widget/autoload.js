e7c9568ff51c58435968d0c6c5283a5fdc0ca73e
rockbenben <qingwhat@gmail.com>
2022-10-12 12:51:49 +0000

    Deploying to gh-pages from @ rockbenben/LearnData@8b9888bb849116c887c588e0a19ed5fa999a8336 🚀

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -36,7 +36,7 @@ if (screen.width >= 768) {
 			//apiPath: "https://live2d.fghrsh.net/api/",
 			//cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/"
 			//使用 api 才能完美换装
-			apiPath: "https://newzone.top/live2d_api/"
+			apiPath: "https://newzone.top/live2d-widget/live2d_api/"
 			//cdnPath: live2d_path + "live2d_api/"
 		});
 	});
