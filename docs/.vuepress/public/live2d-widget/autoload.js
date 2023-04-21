23bbea0c941805a58a92d70c7bdb6f610fba69fe
rockbenben <qingwhat@gmail.com>
2023-04-21 22:21:32 +0800

    style: update style of live2d-widget

--- docs/.vuepress/public/live2d-widget/autoload.js
+++ docs/.vuepress/public/live2d-widget/autoload.js
@@ -1,4 +1,4 @@
-// 注意：live2d_path 参数应使用绝对路径
+// live2d_path 参数建议使用绝对路径
 //const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
 const live2d_path = "/live2d-widget/";
 
@@ -31,13 +31,15 @@ if (screen.width >= 768) {
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
+		// 配置选项的具体用法见 README.md
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://live2d-widget.netlify.app/live2d_api/",
-			//cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/"
+			//apiPath: "https://live2d.fghrsh.net/api/",
+			//cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
 			//使用 api 才能完美换装，但测试 netlify 和 Vercel api 部署有问题，推荐自建或使用本地 cdn
-			apiPath: "https://newzone.top/live2d-widget/live2d_api/"
-			//cdnPath: live2d_path + "live2d_api/"
+			apiPath: "https://live2d.aishort.top/",
+			//cdnPath: live2d_path + "live2d_api/",
+			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
 }
