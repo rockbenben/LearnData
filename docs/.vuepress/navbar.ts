b9b92221f08719c4861a5fae14f2c630a08c422b
rockbenben <qingwhat@gmail.com>
2023-05-06 05:23:50 +0800

    docs: add email contact

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -55,6 +55,7 @@ export default navbar([
     icon: "advance",
     children: [
       { text: "微信", icon: "wechat", link: "https://img.newzone.top/wechat.svg" },
+      { text: "Email", icon: "alias", link: "mailto:learndata@newzone.top" },
       { text: "Discord", icon: "group", link: "https://discord.gg/PZTQfJ4GjX" },
       { text: "RSS", icon: "rss", link: "https://newzone.top/rss.xml" },
     ],
