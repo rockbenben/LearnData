e970f5f1353b4c2c5b1ef70a678812611bc4d46c
rockbenben <qingwhat@gmail.com>
2023-11-29 02:00:23 +0000

    Deploying to gh-pages from @ rockbenben/LearnData@ca37c6e2db4784102123e4376a16de5f347a61f3 🚀

---
+++ assets/pageview-11ad3060.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./app-d04775fe.js";const r=async()=>{try{const{pageviewCount:t}=await n(()=>import("./app-d04775fe.js").then(e=>e.M),[]);return t({serverURL:"https://waline.newzone.top"})}catch{console.error("@waline/client is not installed!");return}};export{r as updatePageview};
