c463a65fb47ef03f7ca1638a0414c5f767c4d4e6
rockbenben <qingwhat@gmail.com>
2024-01-11 21:12:22 +0000

    Deploying to gh-pages from @ rockbenben/LearnData@fdc9de38b53838fb6d34355d24e3cf782feb4f26 🚀

---
+++ assets/pageview-XSy_QtGX.js
@@ -0,0 +1,7 @@
+import{_ as n}from"./app-Gs_rGsV4.js";var o={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://waline.newzone.top",reaction:["https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-y/twemoji/13.1.0/72x72/1f44d.png","https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-y/twemoji/13.1.0/72x72/1f44f.png","https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-y/twemoji/13.1.0/72x72/1f60e.png","https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-y/twemoji/13.1.0/72x72/1f602.png","https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-y/twemoji/13.1.0/72x72/1f635-200d-1f4ab.png"]};const c=async()=>{try{const{pageviewCount:e}=await n(()=>import("./app-Gs_rGsV4.js").then(t=>t.M),__vite__mapDeps([]));return e({serverURL:o.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{c as updatePageview};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
