f73a70bd6c6f768a78eb158f8f07e71ea085bb7e
MasonKai <39311653+MasonKai@users.noreply.github.com>
2024-04-03 19:36:41 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -13,7 +13,7 @@ export default defineUserConfig({
   description: "开源工具、效率方法、心理学探索的自我提升笔记，记录并输出一切能让自己提升的知识。",
 
   // 网站路径默认为主域名。如果网站部署在子路径下，比如 xxx.com/yyy，那么 base 应该被设置为 "/yyy/"
-  base: "/",
+  base: "/kfyze.github.io",
 
   theme,
   // 是否开启页面预拉取，如果服务器宽带足够，可改为 true，会提升其他页面加载速度
