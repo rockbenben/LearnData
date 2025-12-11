61294258e60bb73fda512685d334f329f6ec42e3
rockbenben <qingwhat@gmail.com>
2025-12-11 13:33:58 +0800

    Update navbar.ts

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -4,7 +4,7 @@ import { navbar } from "vuepress-theme-hope";
 // https://fontawesome.com/search?m=free&o=r
 // 专题话题的路径需在尾部添加 /，否则有可能出现链接错误。比如下方「生活」中的 baby/
 export default navbar([
-  { text: "博客", icon: "fa6-solid:blog", link: "/blog" },
+  { text: "博客", icon: "fa6-solid:blog", link: "/blog.md" },
   {
     text: "应用",
     icon: "fa6-solid:bars-staggered",
@@ -41,12 +41,21 @@ export default navbar([
     icon: "fa6-solid:toolbox",
     children: [
       {
+        text: "Tools By AI",
+        icon: "fa6-solid:language",
+        link: "https://tools.newzone.top/zh",
+      },
+      {
         text: "ChatGPT SC",
         icon: "fa6-solid:bolt",
         link: "https://www.aishort.top/",
       },
-      { text: "IMGPrompt", icon: "fa6-solid:image", link: "https://prompt.newzone.top/" },
-      { text: "多语言翻译", icon: "fa6-solid:language", link: "https://tools.newzone.top/json-translate" },
+      { text: "IMGPrompt", icon: "fa6-solid:image", link: "https://prompt.newzone.top/app/zh" },
+      {
+        text: "ChatBox",
+        icon: "ri:chat-smile-ai-fill",
+        link: "https://chat.newzone.top/?lang=zh",
+      },
       {
         text: "工具收藏",
         icon: "fa6-solid:bars",
