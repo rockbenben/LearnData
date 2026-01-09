c44ccc6c809b8051001b3e2a3ec3eaf32ba0984e
rockbenben <qingwhat@gmail.com>
2026-01-09 11:18:44 +0000

    Deploying to gh-pages from @ rockbenben/LearnData@5ec173999b0c5e35bef6969f94fb94e8158385d9 🚀

--- live2d-widget/rollup.config.js
+++ live2d-widget/rollup.config.js
@@ -1,1 +1,38 @@
-import{nodeResolve}from"@rollup/plugin-node-resolve";import{createFilter}from"@rollup/pluginutils";function string(e={}){if(!e.include)throw Error("include option should be specified");const i=createFilter(e.include,e.exclude);return{name:"string",transform(e,n){if(i(n))return{code:`export default ${JSON.stringify(e)};`,map:{mappings:""}}},renderChunk:(e,i,n={})=>"/*!\n * Live2D Widget\n * https://github.com/stevenjoezhang/live2d-widget\n */\n"+e}}export default{input:"src/waifu-tips.js",plugins:[nodeResolve(),string({include:"**/*.svg"})]};
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { createFilter } from "@rollup/pluginutils";
+
+function string(opts = {}) {
+    if (!opts.include) {
+        throw Error("include option should be specified");
+    }
+
+    const filter = createFilter(opts.include, opts.exclude);
+
+    return {
+        name: "string",
+
+        transform(code, id) {
+            if (filter(id)) {
+                return {
+                    code: `export default ${JSON.stringify(code)};`,
+                    map: { mappings: "" }
+                };
+            }
+        },
+
+        renderChunk(code, chunk, outputOptions = {}) {
+            return `/*!
+ * Live2D Widget
+ * https://github.com/stevenjoezhang/live2d-widget
+ */
+` + code;
+        }
+    };
+}
+
+export default {
+    input: "src/waifu-tips.js",
+    plugins: [nodeResolve(), string({
+        include: "**/*.svg",
+    })]
+};
